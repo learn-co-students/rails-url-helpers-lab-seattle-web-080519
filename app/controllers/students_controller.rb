2f7c2577f885f2888c4a6034d2aa9b212edf39b5
github username <github email address>
2019-09-01 00:54:19 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,8 +6,22 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
   end
 
+  def activate
+    @student = Student.find(params[:id])
+    if @student.active 
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.active 
+    @student.save
+
+    redirect_to student_path(@student)
+    #in rails, it's "redirect_to", not 'redirect to' (use the underscore!)
+  end
   private
 
     def set_student
